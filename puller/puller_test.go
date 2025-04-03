dd7174797962abef645be94f544aca170bed45b6
Chris Ramón <rchristian.ramon@gmail.com>
2025-04-03 16:53:47 -0500

    puller: adds Puller.Pull method unit tests

--- puller/puller_test.go
+++ puller/puller_test.go
@@ -1,6 +1,10 @@
 package puller
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/graphql-go/compatibility-base/types"
+)
 
 func TestNew(t *testing.T) {
 	puller := New()
@@ -8,6 +12,29 @@ func TestNew(t *testing.T) {
 	expected := &Puller{}
 
 	if puller == nil {
+		t.Fatalf("expected: %+v, got: nil", expected)
+	}
+}
+
+func TestPullerPull(t *testing.T) {
+	puller := New()
+
+	expected := &PullResult{}
+
+	params := &PullParams{
+		Implementation: &types.Repository{
+			Name:          "graphql-graphql-js",
+			URL:           "https://github.com/graphql/graphql-js",
+			ReferenceName: "v0.6.0",
+			Dir:           "./repos/graphql-graphql-js/",
+		},
+	}
+	result, err := puller.Pull(params)
+	if err != nil {
+		t.Fatalf("expected: nil, got: %v", err)
+	}
+
+	if result != nil {
 		t.Fatalf("expected: %+v, got: nil", expected)
 	}
 }
