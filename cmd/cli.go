0200a5f7fb19306f18679ef445de64e69ebe0c85
Chris Ramón <rchristian.ramon@gmail.com>
2025-04-11 15:40:43 -0500

    cmd: adds New method and parameterizes bubbletea dependency

--- cmd/cli.go
+++ cmd/cli.go
@@ -8,11 +8,18 @@ import (
 
 // CLI represents the command line interface component.
 type CLI struct {
+	bubbletea *bubbletea.BubbleTea
+}
+
+type NewParams struct {
+	Bubbletea *bubbletea.BubbleTea
 }
 
 // New returns a pointer to the `CLI` struct.
-func New() *CLI {
-	return &CLI{}
+func New(p *NewParams) *CLI {
+	return &CLI{
+		bubbletea: p.Bubbletea,
+	}
 }
 
 // RunResult is the result of executing the run method.
@@ -28,14 +35,7 @@ type RunParams struct {
 
 // Run runs the CLI application and returns the result.
 func (c *CLI) Run(p *RunParams) (*RunResult, error) {
-	bt := bubbletea.New(&bubbletea.Params{
-		Choices: p.Choices,
-		UI: bubbletea.UIParams{
-			Header: p.Header,
-		},
-	})
-
-	btRunResult, err := bt.Run()
+	btRunResult, err := c.bubbletea.Run()
 	if err != nil {
 		return nil, fmt.Errorf("failed to run: %w", err)
 	}
