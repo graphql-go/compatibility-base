061dc43c37a6915db87684a57f9c6bd7fe884302
Chris Ramón <rchristian.ramon@gmail.com>
2025-07-17 13:21:39 -0500

    bin/dev.sh: updates paths

--- bin/dev.sh
+++ bin/dev.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ "$@" == "single" ]]; then
-  go run cmd/internal/single_model.go
+  go run cmd/internal/single/single_model.go
 elif [[ "$@" == "multiple" ]]; then
-  go run cmd/internal/multiple_model.go
+  go run cmd/internal/multiple/multiple_model.go
 fi
